c068f16bb9907e44b2d6c960c690866ab841eff0
sakiso <sakis4ssy@gmail.com>
2020-07-12 18:24:08 +0900

    診断開始→Q01の遷移のみ実装

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,36 @@
 <template>
   <div id="app">
- <v-app>
+  <v-app>
+
+
   <v-app-bar color="teal darken-1" dark app>
      <v-toolbar-title>MESHI nator</v-toolbar-title>
   </v-app-bar>
   <br><br><br><br>
 
-  <v-container fluid>
+  <!-- <v-container fluid>
     <v-row> 
      <v-col>
 
       <threeRadio></threeRadio>
 
-    </v-col>
-  </v-row>
+     </v-col>
+   </v-row>
+  </v-container> -->
 
-  </v-container>
-  
+  <router-view></router-view>
 
+ 
   </v-app>
   </div>
 </template>
 
 <script>
-import threeRadio from "./components/threeRadio";
+//import threeRadio from "./components/threeRadio";
 export default {
   name: 'App',
   components: {
-    threeRadio
+//    threeRadio
   }
 }
 </script>
-
-<style>
-</style>
